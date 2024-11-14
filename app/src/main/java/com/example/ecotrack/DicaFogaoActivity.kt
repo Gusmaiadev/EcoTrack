1b9de0683988962e9b87ed985bced0589a57e113
Gustavo Maia <gustadev@yahoo.com>
2024-11-14 20:48:07 -0300

    Finalização telas Dicas

---
+++ app/src/main/java/com/example/ecotrack/DicaFogaoActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ecotrack
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DicaFogaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_dica_fogao)
+
+    }
+}
